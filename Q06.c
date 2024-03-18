7ed9ae0c9136e84b7f403cc8536e5c44edf01629
Franklin Correia <franklin.correia.101@ufrn.edu.br>
2024-03-18 07:24:30 -0300

    Questao_06

---
+++ Q06.c
@@ -0,0 +1,3 @@
+//6. Assumindo que pulo [ ] é um vetor do tipo int, quais das seguintes expressões referenciam o valor do terceiro elemento do vetor?
+
+(pulo + 2) se refere ao terceiro elemento do vetor.
