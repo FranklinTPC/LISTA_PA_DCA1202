75340942cf2d67a70763a7e3e23d7bf703028ffa
Franklin Correia <franklin.correia.101@ufrn.edu.br>
2024-03-19 18:35:10 -0300

    Questao_06

--- Q06.c
+++ Q06.c
@@ -1,3 +1,18 @@
 //6. Assumindo que pulo [ ] é um vetor do tipo int, quais das seguintes expressões referenciam o valor do terceiro elemento do vetor?
 
-(pulo + 2) se refere ao terceiro elemento do vetor.
+#include <stdio.h>
+
+int main(){
+    int pulo[4]; // Declaração de um vetor de inteiros com 4 posições.
+
+    *(pulo + 2); // Acessa o terceiro elemento do vetor.
+
+    *(pulo + 4); // Tenta acessar um elemento fora dos limites do vetor 'erro'.
+
+    pulo + 4; // Calcula o endereço do quinto elemento do vetor.
+
+    pulo + 2; // Calcula o endereço do terceiro elemento do vetor.
+
+    return 0;
+}
+
